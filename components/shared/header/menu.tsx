eff36fe9673aaa3841848d2bbc66a1a63fb69911
Rajesh K <rajeshkrk1996@gmail.com>
2025-02-23 14:58:02 +0530

    Added User Icon and Sign Out
    Added User Icon and Sign Out in Home page

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components/ui/button";
 import ModeToggle from "./mode-toggle";
 import Link from "next/link";
-import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
+import { EllipsisVertical, ShoppingCart } from "lucide-react";
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import UserButton from "./user-button";
 
 const Menu = () => {
     return (
@@ -15,12 +16,14 @@ const Menu = () => {
                 Cart
                 </Link>
             </Button>
-            <Button asChild>
+            {/* <Button asChild>
                 <Link href='/sign-in'>
                 <UserIcon />
                 Sign In
                 </Link>
-            </Button>
+            </Button> */}
+            {/* Replace signin button with UserButton  */}
+            <UserButton />
             </nav>
             <nav className="md:hidden">
                 <Sheet>
@@ -37,6 +40,7 @@ const Menu = () => {
                                 <ShoppingCart />
                             </Link>
                         </Button>
+                        <UserButton />
                         <SheetDescription></SheetDescription>
                     </SheetContent>
                 </Sheet>
